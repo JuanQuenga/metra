d02670b4560cd52c3879d506a2f2c0c240cfb3e5
Juan Quenga <juanquenga@gmail.com>
2022-11-05 07:16:07 -0400

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,66 @@
 const Navbar = () => {
+  const links = [
+    {
+      title: "Community",
+      href: "/community",
+    },
+    {
+      title: "Affiliates",
+      href: "/community",
+    },
+    {
+      title: "Latest Issue",
+      href: "/community",
+    },
+    {
+      title: "Release Schedule",
+      href: "/community",
+    },
+    {
+      title: "Advertising",
+      href: "/community",
+    },
+    {
+      title: "Contact",
+      href: "/community",
+    },
+  ];
   return (
-    <nav className="flex flex-row justify-between h-[200px]">
-      <div className="font-metra text-5xl">METRA</div>
-      <NavLink color="bg-red-600" />
-      <NavLink color="bg-red-600" />
-      <NavLink color="bg-red-600" />
-      <NavLink color="bg-red-600" />
-      <NavLink color="bg-red-600" />
-      <NavLink color="bg-red-600" />
-    </nav>
+    <header className="group h-20 bg-stone-800 ">
+      <div className="flex items-center">
+        <div className="flex-grow pl-4 ">
+          <span className="font-metra text-5xl text-transparent drop-shadow-lg bg-rainbow bg-clip-text animate-rainbow">
+            METRA
+          </span>
+        </div>
+        <nav className="items-center flex h-20 whitespace-nowrap gap-4 pr-4">
+          {links.map((link) => (
+            <NavLink key={link.href} title={link.title} href={link.href} />
+          ))}
+        </nav>
+      </div>
+      <div className="flex w-full h-2 group-hover:h-4 transition-all duration-300 ease-in-out">
+        <div className="w-1/6 h-full bg-red-600" />
+        <div className="w-1/6 h-full bg-orange-600" />
+        <div className="w-1/6 h-full bg-yellow-600" />
+        <div className="w-1/6 h-full bg-green-600" />
+        <div className="w-1/6 h-full bg-blue-600" />
+        <div className="w-1/6 h-full bg-violet-600" />
+      </div>
+    </header>
   );
 };
 
 type NavLinkProps = {
-  color: string;
+  title: string;
+  href: string;
 };
-const NavLink = ({ color }: NavLinkProps) => {
-  return <div className={"basis-1/6 " + color}>Link</div>;
+const NavLink = ({ title = "N/A", href = "/" }: NavLinkProps) => {
+  return (
+    <a href={href} className={"text-white basis-1/6 "}>
+      {title}
+    </a>
+  );
 };
 
 export default Navbar;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,11 +3,32 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      animation: {
+        rainbow: "rainbow 15s ease infinite",
+      },
+      keyframes: {
+        rainbow: {
+          "0%": {
+            "background-size": "600% 600%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "600% 600%",
+            "background-position": "right center",
+          },
+          "100%": {
+            "background-size": "600% 600%",
+            "background-position": "left center",
+          },
+        },
+      },
       fontFamily: {
         metra: ["Monoton", "sans-serif"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        rainbow:
+          "linear-gradient(90deg,#e11d48 0%,#ea580c 20%,#ca8a04 40%,#16a34a 60%,#2563eb 80%,#7c3aed 100%)",
       },
     },
   },
